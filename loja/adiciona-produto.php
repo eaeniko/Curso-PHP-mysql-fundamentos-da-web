42e94758b4c093af3d8a75bcb524b2ca6d57a6f9
NikolaiCinotti <agencianiko@hotmail.com>
2016-09-05 02:46:36 -0300

    criando conexões e adicionando dados no mysql
    mysqli_connect e  _query

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -2,9 +2,20 @@
 	<?php 
 	$nome = $_GET["nome"];
 	$preco = $_GET["preco"];
+	$conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
+	$query = "insert into produtos (nome, preco) values('{$nome}', {$preco})";
+
+	if(mysqli_query($conexao, $query)) { ?>
+
+		<p class="alert-success">Produto <?php echo $nome; ?>, <?php echo $preco ?> reais adicionado com sucesso!</p>
+	<?php } else { ?>
+		
+		<p class="alert-danger">Produto <?php echo $nome; ?>,  não foi adicionado. Verifique novamente</p>
+
+	<?php
+	}
 
 	?>
 
-	<p class="alert-success">Produto <?php echo $nome; ?>, <?php echo $preco ?> reais adicionado com sucesso!</p>
 <?php include("footer.php"); ?>
